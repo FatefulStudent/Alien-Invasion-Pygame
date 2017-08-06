df4274f75cc8fd1a655a7c177766f7588a4119de
Vlad <vladislavvladislavovich@gmail.com>
2017-08-06 17:08:02 +0300

    Dropped the biggest chunk of the code
    At this stage game already can be played: player can move their ship as well as shoot bullets, aliens are destroyed after contact with the bullet and the fleet would reappear after it's been destroyed.

---
+++ settings.py
@@ -0,0 +1,22 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize game settings"""
+        # Screen Settings:
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
+        
+        # Bullet settings
+        self.bullet_speed_factor = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 100, 100, 100
+        self.bullets_allowed = 3
+        
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
